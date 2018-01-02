013ddac54324bf54c93df910abec8698c34f2593
Kyle Nordbo <knordbo@atlassian.com>
2018-01-02 21:37:42 +0100

    Add back sourceSets block - Add copyright header - Remove empty lines

--- src/test/kotlin/com/curiouscreature/kotlin/math/MatrixTest.kt
+++ src/test/kotlin/com/curiouscreature/kotlin/math/MatrixTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 Romain Guy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.curiouscreature.kotlin.math
 
 import org.junit.Assert
@@ -5,7 +21,6 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 class MatrixTest {
-
     @Test
     fun `Mat3 identity`() {
         assertEquals(
@@ -284,5 +299,4 @@ class MatrixTest {
          */
         private fun Int.floatArray() = FloatArray(this) { (it + 1).toFloat() }
     }
-
 }
