5d875d31e77166166121fd55aa2f851ddf74b15d
Denis Konoplev <dekonoplyov@gmail.com>
2018-07-09 00:43:29 +0300

    Mat4 inverse without extra allocation

--- src/main/kotlin/com/curiouscreature/kotlin/math/Matrix.kt
+++ src/main/kotlin/com/curiouscreature/kotlin/math/Matrix.kt
@@ -387,77 +387,70 @@ fun transpose(m: Mat4) = Mat4(
 )
 fun inverse(m: Mat4): Mat4 {
     val result = Mat4()
-    val pairs = FloatArray(12)
 
-    pairs[0]  = m.z.z * m.w.w
-    pairs[1]  = m.w.z * m.z.w
-    pairs[2]  = m.y.z * m.w.w
-    pairs[3]  = m.w.z * m.y.w
-    pairs[4]  = m.y.z * m.z.w
-    pairs[5]  = m.z.z * m.y.w
-    pairs[6]  = m.x.z * m.w.w
-    pairs[7]  = m.w.z * m.x.w
-    pairs[8]  = m.x.z * m.z.w
-    pairs[9]  = m.z.z * m.x.w
-    pairs[10] = m.x.z * m.y.w
-    pairs[11] = m.y.z * m.x.w
+    var pair0  = m.z.z * m.w.w
+    var pair1  = m.w.z * m.z.w
+    var pair2  = m.y.z * m.w.w
+    var pair3  = m.w.z * m.y.w
+    var pair4  = m.y.z * m.z.w
+    var pair5  = m.z.z * m.y.w
+    var pair6  = m.x.z * m.w.w
+    var pair7  = m.w.z * m.x.w
+    var pair8  = m.x.z * m.z.w
+    var pair9  = m.z.z * m.x.w
+    var pair10 = m.x.z * m.y.w
+    var pair11 = m.y.z * m.x.w
 
-    result.x.x = pairs[0] * m.y.y + pairs[3] * m.z.y + pairs[4] * m.w.y
-    result.x.x -= pairs[1] * m.y.y + pairs[2] * m.z.y + pairs[5] * m.w.y
-    result.x.y = pairs[1] * m.x.y + pairs[6] * m.z.y + pairs[9] * m.w.y
-    result.x.y -= pairs[0] * m.x.y + pairs[7] * m.z.y + pairs[8] * m.w.y
-    result.x.z = pairs[2] * m.x.y + pairs[7] * m.y.y + pairs[10] * m.w.y
-    result.x.z -= pairs[3] * m.x.y + pairs[6] * m.y.y + pairs[11] * m.w.y
-    result.x.w = pairs[5] * m.x.y + pairs[8] * m.y.y + pairs[11] * m.z.y
-    result.x.w -= pairs[4] * m.x.y + pairs[9] * m.y.y + pairs[10] * m.z.y
-    result.y.x = pairs[1] * m.y.x + pairs[2] * m.z.x + pairs[5] * m.w.x
-    result.y.x -= pairs[0] * m.y.x + pairs[3] * m.z.x + pairs[4] * m.w.x
-    result.y.y = pairs[0] * m.x.x + pairs[7] * m.z.x + pairs[8] * m.w.x
-    result.y.y -= pairs[1] * m.x.x + pairs[6] * m.z.x + pairs[9] * m.w.x
-    result.y.z = pairs[3] * m.x.x + pairs[6] * m.y.x + pairs[11] * m.w.x
-    result.y.z -= pairs[2] * m.x.x + pairs[7] * m.y.x + pairs[10] * m.w.x
-    result.y.w = pairs[4] * m.x.x + pairs[9] * m.y.x + pairs[10] * m.z.x
-    result.y.w -= pairs[5] * m.x.x + pairs[8] * m.y.x + pairs[11] * m.z.x
+    result.x.x  = pair0 * m.y.y + pair3 * m.z.y + pair4 * m.w.y
+    result.x.x -= pair1 * m.y.y + pair2 * m.z.y + pair5 * m.w.y
+    result.x.y  = pair1 * m.x.y + pair6 * m.z.y + pair9 * m.w.y
+    result.x.y -= pair0 * m.x.y + pair7 * m.z.y + pair8 * m.w.y
+    result.x.z  = pair2 * m.x.y + pair7 * m.y.y + pair10 * m.w.y
+    result.x.z -= pair3 * m.x.y + pair6 * m.y.y + pair11 * m.w.y
+    result.x.w  = pair5 * m.x.y + pair8 * m.y.y + pair11 * m.z.y
+    result.x.w -= pair4 * m.x.y + pair9 * m.y.y + pair10 * m.z.y
+    result.y.x  = pair1 * m.y.x + pair2 * m.z.x + pair5 * m.w.x
+    result.y.x -= pair0 * m.y.x + pair3 * m.z.x + pair4 * m.w.x
+    result.y.y  = pair0 * m.x.x + pair7 * m.z.x + pair8 * m.w.x
+    result.y.y -= pair1 * m.x.x + pair6 * m.z.x + pair9 * m.w.x
+    result.y.z  = pair3 * m.x.x + pair6 * m.y.x + pair11 * m.w.x
+    result.y.z -= pair2 * m.x.x + pair7 * m.y.x + pair10 * m.w.x
+    result.y.w  = pair4 * m.x.x + pair9 * m.y.x + pair10 * m.z.x
+    result.y.w -= pair5 * m.x.x + pair8 * m.y.x + pair11 * m.z.x
 
-    pairs[0] = m.z.x * m.w.y
-    pairs[1] = m.w.x * m.z.y
-    pairs[2] = m.y.x * m.w.y
-    pairs[3] = m.w.x * m.y.y
-    pairs[4] = m.y.x * m.z.y
-    pairs[5] = m.z.x * m.y.y
-    pairs[6] = m.x.x * m.w.y
-    pairs[7] = m.w.x * m.x.y
-    pairs[8] = m.x.x * m.z.y
-    pairs[9] = m.z.x * m.x.y
-    pairs[10] = m.x.x * m.y.y
-    pairs[11] = m.y.x * m.x.y
+    pair0 = m.z.x * m.w.y
+    pair1 = m.w.x * m.z.y
+    pair2 = m.y.x * m.w.y
+    pair3 = m.w.x * m.y.y
+    pair4 = m.y.x * m.z.y
+    pair5 = m.z.x * m.y.y
+    pair6 = m.x.x * m.w.y
+    pair7 = m.w.x * m.x.y
+    pair8 = m.x.x * m.z.y
+    pair9 = m.z.x * m.x.y
+    pair10 = m.x.x * m.y.y
+    pair11 = m.y.x * m.x.y
 
-    result.z.x = pairs[0] * m.y.w + pairs[3] * m.z.w + pairs[4] * m.w.w
-    result.z.x -= pairs[1] * m.y.w + pairs[2] * m.z.w + pairs[5] * m.w.w
-    result.z.y = pairs[1] * m.x.w + pairs[6] * m.z.w + pairs[9] * m.w.w
-    result.z.y -= pairs[0] * m.x.w + pairs[7] * m.z.w + pairs[8] * m.w.w
-    result.z.z = pairs[2] * m.x.w + pairs[7] * m.y.w + pairs[10] * m.w.w
-    result.z.z -= pairs[3] * m.x.w + pairs[6] * m.y.w + pairs[11] * m.w.w
-    result.z.w = pairs[5] * m.x.w + pairs[8] * m.y.w + pairs[11] * m.z.w
-    result.z.w -= pairs[4] * m.x.w + pairs[9] * m.y.w + pairs[10] * m.z.w
-    result.w.x = pairs[2] * m.z.z + pairs[5] * m.w.z + pairs[1] * m.y.z
-    result.w.x -= pairs[4] * m.w.z + pairs[0] * m.y.z + pairs[3] * m.z.z
-    result.w.y = pairs[8] * m.w.z + pairs[0] * m.x.z + pairs[7] * m.z.z
-    result.w.y -= pairs[6] * m.z.z + pairs[9] * m.w.z + pairs[1] * m.x.z
-    result.w.z = pairs[6] * m.y.z + pairs[11] * m.w.z + pairs[3] * m.x.z
-    result.w.z -= pairs[10] * m.w.z + pairs[2] * m.x.z + pairs[7] * m.y.z
-    result.w.w = pairs[10] * m.z.z + pairs[4] * m.x.z + pairs[9] * m.y.z
-    result.w.w -= pairs[8] * m.y.z + pairs[11] * m.z.z + pairs[5] * m.x.z
+    result.z.x  = pair0 * m.y.w + pair3 * m.z.w + pair4 * m.w.w
+    result.z.x -= pair1 * m.y.w + pair2 * m.z.w + pair5 * m.w.w
+    result.z.y  = pair1 * m.x.w + pair6 * m.z.w + pair9 * m.w.w
+    result.z.y -= pair0 * m.x.w + pair7 * m.z.w + pair8 * m.w.w
+    result.z.z  = pair2 * m.x.w + pair7 * m.y.w + pair10 * m.w.w
+    result.z.z -= pair3 * m.x.w + pair6 * m.y.w + pair11 * m.w.w
+    result.z.w  = pair5 * m.x.w + pair8 * m.y.w + pair11 * m.z.w
+    result.z.w -= pair4 * m.x.w + pair9 * m.y.w + pair10 * m.z.w
+    result.w.x  = pair2 * m.z.z + pair5 * m.w.z + pair1 * m.y.z
+    result.w.x -= pair4 * m.w.z + pair0 * m.y.z + pair3 * m.z.z
+    result.w.y  = pair8 * m.w.z + pair0 * m.x.z + pair7 * m.z.z
+    result.w.y -= pair6 * m.z.z + pair9 * m.w.z + pair1 * m.x.z
+    result.w.z  = pair6 * m.y.z + pair11 * m.w.z + pair3 * m.x.z
+    result.w.z -= pair10 * m.w.z + pair2 * m.x.z + pair7 * m.y.z
+    result.w.w  = pair10 * m.z.z + pair4 * m.x.z + pair9 * m.y.z
+    result.w.w -= pair8 * m.y.z + pair11 * m.z.z + pair5 * m.x.z
 
-    val det = 1 / (m.x.x * result.x.x + m.y.x * result.x.y + m.z.x * result.x.z + m.w.x * result.x.w)
+    val determinant = m.x.x * result.x.x + m.y.x * result.x.y + m.z.x * result.x.z + m.w.x * result.x.w
 
-    for (i in 0..3) {
-        for (j in 0..3) {
-            result[i][j] *= det
-        }
-    }
-
-    return result
+    return result / determinant
 }
 
 fun scale(s: Float3) = Mat4(Float4(x = s.x), Float4(y = s.y), Float4(z = s.z))
