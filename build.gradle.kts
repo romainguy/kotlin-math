9603ab5d1851c2c33e30e7ccbfc94330fd93412c
Romain Guy <romainguy@curious-creature.com>
2022-10-07 08:40:27 -0700

    Update to Kotlin 1.7.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.konan.target.HostManager
 import java.net.URL
 
 plugins {
-    kotlin("multiplatform") version "1.7.0"
+    kotlin("multiplatform") version "1.7.20"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("org.jetbrains.dokka") version "1.6.0"
     id("maven-publish")
