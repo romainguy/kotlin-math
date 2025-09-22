93ddebc7f081d59318067b4687103439f7cbe485
Sergey C. <sergey.chelombitko@gmail.com>
2025-09-22 20:04:03 +0100

    Fix build after Dokka upgrade (#60)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 import com.vanniktech.maven.publish.SonatypeHost
-import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.konan.target.HostManager
-
-import java.net.URL
 
 plugins {
     kotlin("multiplatform") version "2.1.20"
@@ -65,36 +62,32 @@ kotlin {
     }
 }
 
-tasks {
-    val dokkaHtml by getting(DokkaTask::class) {
-        dokkaSourceSets {
-            configureEach {
-                reportUndocumented.set(false)
-                skipEmptyPackages.set(true)
-                skipDeprecated.set(true)
-                jdkVersion.set(8)
+dokka {
+    dokkaSourceSets.configureEach {
+        reportUndocumented.set(false)
+        skipEmptyPackages.set(true)
+        skipDeprecated.set(true)
+        jdkVersion.set(8)
 
-                // Add Android SDK packages
-                noAndroidSdkLink.set(false)
+        // Add Android SDK packages
+        enableAndroidDocumentationLink.set(true)
 
-                sourceLink {
-                    localDirectory.set(project.file("src/commonMain/kotlin"))
-                    // URL showing where the source code can be accessed through the web browser
-                    remoteUrl.set(URL("https://github.com/romainguy/kotlin-math/blob/main/${project.name}/src/commonMain/kotlin"))
-                    // Suffix which is used to append the line number to the URL. Use #L for GitHub
-                    remoteLineSuffix.set("#L")
-                }
-            }
+        sourceLink {
+            localDirectory.set(project.file("src/commonMain/kotlin"))
+            // URL showing where the source code can be accessed through the web browser
+            remoteUrl.set(uri("https://github.com/romainguy/kotlin-math/blob/main/${project.name}/src/commonMain/kotlin"))
+            // Suffix which is used to append the line number to the URL. Use #L for GitHub
+            remoteLineSuffix.set("#L")
         }
     }
 }
 
-val dokkaHtml by tasks.getting(org.jetbrains.dokka.gradle.DokkaTask::class)
+val dokkaGeneratePublicationHtml by tasks.getting(org.jetbrains.dokka.gradle.tasks.DokkaGeneratePublicationTask::class)
 
 val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
-    dependsOn(dokkaHtml)
+    dependsOn(dokkaGeneratePublicationHtml)
     archiveClassifier.set("javadoc")
-    from(dokkaHtml.outputDirectory)
+    from(dokkaGeneratePublicationHtml.outputDirectory)
 }
 
 mavenPublishing {
